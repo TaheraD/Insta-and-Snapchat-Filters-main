94cfb918265bc949e74839be24932c69f81af1e9
TaheraD <110984242+TaheraD@users.noreply.github.com>
2022-08-24 18:41:59 +0530

    Update blur.py

--- Insta-and-Snapchat-Filters-main/insta filter/blur.py
+++ Insta-and-Snapchat-Filters-main/insta filter/blur.py
@@ -1,7 +1,6 @@
 import cv2
 import matplotlib.pyplot as plt
 im = cv2.imread(r'C:\Users\pc\Downloads\Insta-and-Snapchat-Filters-main\Insta-and-Snapchat-Filters-main\insta filter\house.jpeg')
-#dst = cv2.GaussianBlur(im,(5,5),0)
-dst = cv2.rotate(im, cv2.ROTATE_90_CLOCKWISE)
-cv2.imshow('rotated',dst)
-#plt.show()
+dst = cv2.GaussianBlur(im,(5,5),cv2.BORDER_DEFAULT)
+plt.imshow(dst)
+plt.show()
